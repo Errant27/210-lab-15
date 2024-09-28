2b5e4295d801d3ef5f158fb8207629ded80300f1
Errant27 <kneil.sgo@gmail.com>
2024-09-28 16:47:08 -0700

    commit 6

--- 210-lab-15/main.cpp
+++ 210-lab-15/main.cpp
@@ -9,51 +9,50 @@ const int FILE_SIZE = 4;
 class Movie
 {
 private:
-    string screenWriter;
-    int releaseYear;
     string title;
+    int releaseYear;
+    string screenWriter;
     
 public:
-    void setScreenWriter(string s) { screenWriter = s; }    // Setter Functions
+    void setTitle(string t) { title = t; }    // Setter Functions
     void setReleaseYear(int r) { releaseYear = r; }
-    void setTitle(string t) { title = t; }
+    void setScreenWriter(string s) { screenWriter = s; }
     
-    string getScreenWriter() { return screenWriter; }    // Getter Functions
+    string getTitle() { return title; }    // Getter Functions
     int getReleaseYear() { return releaseYear; }
-    string getTitle() { return title; }
+    string getScreenWriter() { return screenWriter; }
     
     void print_Movie() {    // Print Function
-        cout << screenWriter << endl;
-        cout << releaseYear << endl;
-        cout << title << endl;
+        cout << "Title: " << title << endl;
+        cout << "Release year: " << releaseYear << endl;
+        cout << "Screenwriter: " << screenWriter << endl;
     }
-}; // End of class
+};     // End of class
 
 int main()
 {
     ifstream reader;
     string fileName = ("TopMovies.txt");
     vector<Movie> movieVector;
-    string s;
     string t;
-    Movie m;
     int n;
+    string s;
+    Movie m;
     
     
     reader.open(fileName);
-    
     if (!reader) {
         cout << "ERROR. File not opened" << endl;
     }
     else {
         for (int i = 0; i < FILE_SIZE; i++) {
-            getline(reader, s);
-            m.setScreenWriter(s);
+            getline(reader, t);
+            m.setTitle(t);
             reader >> n;
             m.setReleaseYear(n);
-            reader >> t;
-            m.setTitle(t);
-            
+            reader >> s;
+            m.setScreenWriter(s);
+           
             reader.ignore();    // Xcode seems to require file reader object be cleared through subsequent loop runs in order to store new values
             reader.clear();     // (Ex: Loop run 1, store ifstream, store in vector, clear ifstream > Loop run 2, store ifstream, store in vector, clear ifstream. etc)
             
